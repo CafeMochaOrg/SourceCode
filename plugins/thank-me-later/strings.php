4b7faca5f3fe43e9c310c650917c443655471fdc
Rajat Bhageria <rajat1996@gmail.com>
2014-09-15 14:15:00 -0400

    CafeMocha Source Code
    Contains only cafe mocha theme source code

--- plugins/thank-me-later/strings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Some extra strings I want translated for description, name, etc.
-
-__("Thank Me Later", "bbpp-thankmelater");
-__("Send a 'thank you' email to your blog's commenters.", "bbpp-thankmelater");
